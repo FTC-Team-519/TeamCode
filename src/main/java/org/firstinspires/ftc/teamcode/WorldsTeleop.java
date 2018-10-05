9ecf34b2c6405e08c250e2ded685c8c2f0dd8ed0
DragonDMS <kbcrumble@gmail.com>
2018-10-04 21:01:42 -0400

    Basic code for demo robot. Ported code from Worlds Robot.

--- src/main/java/org/firstinspires/ftc/teamcode/WorldsTeleop.java
+++ src/main/java/org/firstinspires/ftc/teamcode/WorldsTeleop.java
@@ -1,33 +1,11 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -56,27 +34,46 @@ import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="TeleOp", group="Iterative Opmode")
+@TeleOp(name="Worlds", group="Iterative Opmode")
 //@Disabled
-public class Teleop extends OpMode
+public class WorldsTeleop extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor frontLeft = null;
-    private DcMotor frontRight = null;
-    private DcMotor backLeft = null;
-    private DcMotor backRight = null;
+
+    private DcMotor shooter;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor midCollector;
+    private DcMotor frontCollector;
+    private DcMotor topCollector;
+    private ColorSensor color;
+    private OpticalDistanceSensor ods;
     private float x;
     private float y;
     private float z;
-    private static final double MAX_SPEED = 1.0d;
 
+    private int sillyCounter = 0;
 
     private double[] motorPowers = new double[4];
     private static final int FRONT_LEFT  = 0;
     private static final int FRONT_RIGHT = 1;
     private static final int BACK_LEFT   = 2;
     private static final int BACK_RIGHT  = 3;
+
+    private static final float DEAD_ZONE = 0.2f;
+    private static final double MAX_SPEED = 1.0d;
+
+    int previousTickCount = 0;
+
+
+    boolean keepShooterSpinning = false;
+    double SHOOTER_REVERSE_SPEED = -0.3d;
+    double SHOOTER_FORWARD_SPEED = 1.0d;
+    double currentShooterSpeed = 0.0d;
+
     boolean flipped = false;
     /*
      * Code to run ONCE when the driver hits INIT
@@ -95,6 +92,7 @@ public class Teleop extends OpMode
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
 
+
         frontLeft  = hardwareMap.get(DcMotor.class, "motor1");
         frontRight = hardwareMap.get(DcMotor.class, "motor2");
         backLeft = hardwareMap.get(DcMotor.class, "motor3");
@@ -102,11 +100,15 @@ public class Teleop extends OpMode
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
 
+        topCollector = hardwareMap.dcMotor.get("topCollector");
+        midCollector = hardwareMap.dcMotor.get("feeder");
+        frontCollector = hardwareMap.dcMotor.get("collector");
+        shooter = hardwareMap.dcMotor.get("shooter");
+
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
 
         // Tell the driver that initialization is complete.
-        telemetry.addData("Status", "Initialized");
     }
 
     /*
@@ -123,8 +125,9 @@ public class Teleop extends OpMode
     @Override
     public void start() {
 
-     //   getTelemetryUtil().addData("Start", getClass().getSimpleName() + " Start.");
-      //  getTelemetryUtil().sendTelemetry();
+
+        //   getTelemetryUtil().addData("Start", getClass().getSimpleName() + " Start.");
+        //  getTelemetryUtil().sendTelemetry();
         runtime.reset();
 
 
@@ -136,8 +139,9 @@ public class Teleop extends OpMode
      */
     @Override
     public void loop() {
-
         Gamepad driver = gamepad1;
+        Gamepad gunner = gamepad2;
+
         updateJoyStickValues();
 
         if (driver.y) {
@@ -180,40 +184,124 @@ public class Teleop extends OpMode
         frontLeft.setPower(reducePower(motorPowers[FRONT_LEFT]));
         backRight.setPower(reducePower(motorPowers[BACK_RIGHT]));
         backLeft.setPower(reducePower(motorPowers[BACK_LEFT]));
+/*
+        if (sillyCounter > 25) {
+            sillyCounter = 0;
+            getTelemetryUtil().addData("Start", "fR: " + frontRight.getPower() +
+                    ", fL: " + frontLeft.getPower() +
+                    ", bR: " + backRight.getPower() +
+                    ", bL: " + backLeft.getPower());
+            getTelemetryUtil().sendTelemetry();
+        }
+        else {
+            ++sillyCounter;
+        }
+*/
+        // Forward/backward power is left_stick_y, but forward is -1.0 reading, so invert
 
+//        if (gamepad1.x) {
+//            currPower = 0.1d;
+//        }
+//        else if (gamepad1.y) {
+//            currPower = 0.2d;
+//        }
+//        else if (gamepad1.b) {
+//            currPower = 0.4d;
+//        }
+//        else if (gamepad1.a) {
+//            currPower = 0.6d;
+//        }
+//        else if (gamepad1.left_bumper) {
+//            currPower = 0.8d;
+//        }
+//        else if (gamepad1.right_bumper) {
+//            currPower = 1.0d;
+//        }
 
+        if (gunner.x || gunner.a || gunner.b) {
+            if (gunner.x) {
+                currentShooterSpeed = SHOOTER_REVERSE_SPEED;
+                keepShooterSpinning = false;
+            }
+            else if (gunner.b) {
+                currentShooterSpeed = SHOOTER_FORWARD_SPEED;
+                keepShooterSpinning = false;
+            }
+            else if (gunner.a) {
+                currentShooterSpeed = 0.0d;
+                keepShooterSpinning = false;
+            }
+        }
+        else {
+            if (! keepShooterSpinning) {
+                currentShooterSpeed = 0.0d;
+            }
+            /** Driver potentially saved controls
+             if (driver.x) {
+             currPower = -1.0d;
+             } else if (driver.b) {
+             currPower = 1.0d;
+             }
+             **/
+        }
 
+        // Negative, as the wheel needs to go in reverse direction (could reverse motor actually)
+        shooter.setPower(currentShooterSpeed);
 
+        // Gunner
 
+        if (gunner.right_bumper) {
+            topCollector.setPower(0.6);
+            midCollector.setPower(0.6);
+        }
 
+        /*if (gunner.right_trigger) {
+            midCollector.setPower(0.5);
+        } */
+        else if (gunner.left_bumper) {
+            topCollector.setPower(-0.6);
+            midCollector.setPower(-0.6);
 
+        }
+        else {
+            topCollector.setPower(0.0);
+            midCollector.setPower(0.0);
+        }
 
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        // Driver
+        if (driver.right_bumper) {
+            frontCollector.setPower(-1.0);
+            //midCollector.setPower(0.25);
+        }
+        else if (driver.left_bumper) {
+            frontCollector.setPower(1.0);
+            //midCollector.setPower(-0.25);
+        }
+        else if ((driver.left_trigger > 0) || (driver.right_trigger > 0)){
+            frontCollector.setPower(0.0);
+            //midCollector.setPower(0.0);
+        }
+/*
+        getTelemetryUtil().addData("RGB: ", "" + color.red() + "," + color.green() + "," + color.blue());
+        getTelemetryUtil().addData("ARGB", " " + color.argb());
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
+        if (timerComponent.targetReached(1.0f)) {
+            int currentTicks = shooter.getController().getMotorCurrentPosition(1);
+            float ticksPerSecond = currentTicks - previousTickCount;
+            previousTickCount = currentTicks;
+            getTelemetryUtil().addData("RPM", "ticksPerMinute: " + ticksPerSecond);
+        }
 
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        getTelemetryUtil().sendTelemetry();
 
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        // Might want to have a more effective combination
+        //frontRight.setPower(Range.clip(pwr - x - z, -MAX_SPEED, MAX_SPEED));
+        //frontLeft.setPower(Range.clip(pwr + x + z, -MAX_SPEED, MAX_SPEED));
+        //backRight.setPower(Range.clip(pwr + x - z, -MAX_SPEED, MAX_SPEED));
+        //backLeft.setPower(Range.clip(pwr - x + z, -MAX_SPEED, MAX_SPEED));
+   */
     }
+
 
     private static void normalizeCombinedPowers(double[] motorPowers) {
         double maxAbsPower = 0.0d;
@@ -261,24 +349,25 @@ public class Teleop extends OpMode
 
         return adjustedValue;
     }
-        private static float shapeInput(float input) {
-            float shapedValue = 0.0f;
-            if (input != 0.0f) {
-                if (input < 0.0f) {
-                    shapedValue = input * -input;
-                }
-                else {
-                    shapedValue = input * input;
-                }
+    private static float shapeInput(float input) {
+        float shapedValue = 0.0f;
+        if (input != 0.0f) {
+            if (input < 0.0f) {
+                shapedValue = input * -input;
             }
-
-            return shapedValue;
-
-            // return input * (input < 0.0f ? -input : input);
+            else {
+                shapedValue = input * input;
+            }
         }
+
+        return shapedValue;
+
+        // return input * (input < 0.0f ? -input : input);
+    }
 
     @Override
     public void stop() {
     }
 
 }
+
